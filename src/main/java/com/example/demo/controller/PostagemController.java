f7fb5ed0827f19871ca5c43e8e9797a1334ae507
unknown <vrnakano@gmail.com>
2020-11-04 16:31:50 -0300

    blog pessoal

--- src/main/java/com/example/demo/controller/PostagemController.java
+++ src/main/java/com/example/demo/controller/PostagemController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +40,27 @@ public class PostagemController {
 				.orElse(ResponseEntity.notFound().build()); //not found caso nao exista ou caso tenha erro na requisicao
 				//retorna por padrao optional, nulo ou populado
 	}
+	
+	@GetMapping("/titulo/{titulo}") //responseentity 
+	public ResponseEntity<List<PostagemModel>> GetByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<PostagemModel> post (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<PostagemModel> put (@RequestBody PostagemModel postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
 	
 }
 
